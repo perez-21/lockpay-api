72ac83dc9872253708e8ea80c17035abd51b0375
Perez-salem Tuwan <perezjoshua1811@gmail.com>
2026-07-07 23:17:00 +0100

    handle prisma error in auth service

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,39 +1,48 @@
-const argon2 = require('argon2');
+const argon2 = require("argon2");
 const prismaClient = require("../shared/prismaClient");
 const { Role } = require("../generated/prisma/client");
 const UnauthorizedError = require("../shared/errors/UnauthorizedError");
-const InternalServerError = require('../shared/errors/InternalServerError');
+const InternalServerError = require("../shared/errors/InternalServerError");
+const BadRequestError = require("../shared/errors/BadRequestError");
 
 const register = async (email, password, firstname, lastname, role) => {
-
   const hashedPassword = await hashPassword(password);
-  const user = await prismaClient.user.create({
-    data: {
-      email,
-      password: hashedPassword,
-      firstname,
-      lastname,
-      role,
-    },
-    omit: { password: true },
-  });
-  if (!user) {
-    return new InternalServerError("Something went wrong registering a new user");
+  try {
+    const user = await prismaClient.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        firstname,
+        lastname,
+        role,
+      },
+      omit: { password: true },
+    });
+    if (!user) {
+      return new InternalServerError(
+        "Something went wrong registering a new user",
+      );
+    }
+    return user;
+  } catch (error) {
+    if (error.code === "P2002"){
+      throw new BadRequestError("Email address already taken");
+    }
+    throw error;
   }
-  return user;
 };
 
 const loginWithEmailAndPassword = async (email, password) => {
-  const user = await prismaClient.user.findUnique({ where: { email }});
+  const user = await prismaClient.user.findUnique({ where: { email } });
 
   if (!user) {
     return new UnauthorizedError("Invalid Credentials");
   }
 
-  if (!await verifyPassword(user.password, password)) {
+  if (!(await verifyPassword(user.password, password))) {
     return new UnauthorizedError("Invalid Credentials");
   }
-  
+
   const { password: _, ...userWithoutPassword } = user;
   return userWithoutPassword;
 };
@@ -44,6 +53,6 @@ const hashPassword = async (password) => {
 
 const verifyPassword = async (hash, plainPassword) => {
   return await argon2.verify(hash, plainPassword);
-}
+};
 
 module.exports = { register, loginWithEmailAndPassword };
